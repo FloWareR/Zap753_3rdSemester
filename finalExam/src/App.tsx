4b4d0296a8144cbc89348668bc3d3ba975be8b12
rFloresg <117321494+rFloresg@users.noreply.github.com>
2023-11-09 12:14:37 -0600

    Examen step 1

--- finalExam/src/App.tsx
+++ finalExam/src/App.tsx
@@ -1,15 +1,31 @@
 import {AiOutlinePlus} from 'react-icons/ai'
 import React,{useState, useEffect} from 'react' 
 import Tasks from './components/Tasks'
-import { query, collection, onSnapshot } from 'firebase/firestore'
+import { query, collection, onSnapshot, updateDoc, doc, addDoc, Firestore, deleteDoc } from 'firebase/firestore'
 import { Firebase } from './Firebase'
 
 
 function App() {
   const [tasks,setTasks] = useState([])
+  const [input,setInput] = useState('')
+  console.log(input)
 
 
   //Create a new task
+  const createTask = async (e)=>{
+    e.preventDefault(e)
+    if(input === ''){
+      alert('Please enter a valid task')
+      return
+    }
+    await addDoc(collection(Firebase, 'tasks'),{
+      text: input,
+      completed: false
+    })
+    setInput('')
+
+  }
+
   //Read tasks (from firebase)
   useEffect(()=>{
     const q = query(collection(Firebase, 'tasks'))
@@ -24,23 +40,47 @@ function App() {
   },[])
 
   //Update tasks in firebase
+  const toggleTask = async (tasks) => {
+    await updateDoc(doc(Firebase, 'tasks', tasks.id),{
+      completed: !tasks.completed
+    })
+  }
+
   //Delete tasks
+  const deleteTask = async (id) => {
+    await deleteDoc(doc(Firebase,'tasks',id))
+  }
+
+
 
 
   return (
       <div className='h-screen w-screen p-4 bg-gradient-to-b from-[#B878F0] to-[#78ACF0]'>
-        <div className="bg-slate-100 max-w[500px] w-full m-auto rounded-md shadow-xl p-4">
-          <h3 className="text-3xl font-bold text-center text-gray-800 p-2">My tasks</h3>
-          <form className="flex justify-between">
-            <input className="border p-2 w-full text-xl" type="text" placeholder='Write a new task...'/>
+        <div className="bg-slate-100  max-w-[700px]	w-full m-auto rounded-md shadow-xl p-4">
+          <h3 className="text-4xl font-bold text-center text-gray-800 p-2">My tasks</h3>
+          <form onSubmit={createTask} className="mt-3 flex justify-between">
+            <input 
+              value={input} onChange= {(e) => (setInput(e.target.value))} 
+              className="border p-2 w-full text-xl" 
+              type="text" 
+              placeholder='Write a new task...'
+              />
             <button className="border p-4 ml-2 bg-[#0017C7] text-slate-100"><AiOutlinePlus size={30} /></button>
           </form>
           <ul>
             {tasks.map((tasks, index)=>(
-              <Tasks key={index} tasks = {tasks}/>
+              <Tasks 
+                key={index} 
+                tasks = {tasks} 
+                toggleTask={toggleTask} 
+                deleteTask={deleteTask}
+              />
             ))}
           </ul>
-          <p className='text-center p-2'>You have 2 tasks</p>
+
+          {tasks.length < 1 ? null : 
+          <p className='text-center p-2 font-bold'>{`You have ${tasks.length} pending tasks`}</p>
+}
         </div>
       </div>
   )
--- finalExam/src/components/Tasks.tsx
+++ finalExam/src/components/Tasks.tsx
@@ -1,13 +1,13 @@
 import {FaRegTrashAlt} from 'react-icons/fa'
 
-function Tasks({tasks}) {
+function Tasks({tasks, toggleTask, deleteTask}) {
   return (
     <li className={tasks.completed ? "flex justify-between bg-slate-400 p-4 my-2 capitalize":"flex justify-between bg-slate-200 p-4 my-2 capitalize" }>
       <div className="flex">
-        <input type="checkbox"/>
-        <p className="ml-2 cursor-pointer">{tasks.text}</p>
+        <input onChange={() => toggleTask(tasks)} type="checkbox" checked={tasks.completed ? 'checked' : ''}/>
+        <p onClick={() => toggleTask(tasks)} className={tasks.completed ? "ml-2 cursor-pointer line-through" :"ml-2 cursor-pointer"}>{tasks.text}</p>
       </div>
-      <button className='cursos-pointer flex items-center'>{<FaRegTrashAlt/>}</button>
+      <button onClick={()=> deleteTask(tasks.id)} className='cursos-pointer flex items-center'>{<FaRegTrashAlt/>}</button>
     </li>
   )
 }
